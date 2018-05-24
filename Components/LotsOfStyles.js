e4d308a9ccc7f2b92bac85ed8edaca326b894c65
Scott <scrosehart@gmail.com>
2018-05-24 15:50:06 -0700

    working on style ex

---
+++ Components/LotsOfStyles.js
@@ -0,0 +1,15 @@
+import React, { Componet } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+export default class LotsOfStyles extends Component {
+  render() {
+    return (
+      <View>
+        <Text style={styles.red}>just red</Text>
+        <Text style={styles.bigblue}>just bigblue</Text>
+        <Text style={[styles.bigblue, styles.red]}>bigblue, then red</Text>
+        <Text style={[styles.red, styles.bigblue]}>red, then bigblue</Text>
+      </View>
+    )
+  }
+}
