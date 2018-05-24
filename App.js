4ea9ce2a683c203b5eeb3575ccccb1155226b585
Scott <scrosehart@gmail.com>
2018-05-24 15:11:34 -0700

    some style

--- App.js
+++ App.js
@@ -6,7 +6,10 @@ export default class HelloWorldApp extends Component {
   render() {
     return (
       <View>
-        <Text>Hello World!</Text>
+        <View style={ {marginTop: 50, marginBottom: 20} }>
+          <Text style={ {marginBottom: 2} }>Hello World!</Text>
+          <Text>This is the main App and Components are below.</Text>
+        </View>
         <Bananas />
       </View>
     );
