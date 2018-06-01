d424a9cf29d123087212a6052b6abdc3add1437d
Scott <scrosehart@gmail.com>
2018-05-31 17:47:45 -0700

    adjusting

--- Components/FlatListBasics.js
+++ Components/FlatListBasics.js
@@ -4,7 +4,7 @@ import { FlatList, StyleSheet, Text, View } from 'react-native';
 export default class FlatListBasics extends Component {
   render() {
     return (
-      <Text>Hey</Text>
+      <Text>Hey yoooo</Text>
     );
   }
 }
